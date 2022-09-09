6adcaaa74201ccc942b1a795c2b41db84d9cd03c
Salami Azeez <salamitopeazeez@gmail.com>
2022-09-08 19:24:43 -0700

    Working with C

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - show sizes of datatypes
+ *
+ * Description: Using main function to print sizes
+ * of datatypes
+ * Return: 0
+ */
+int main(void)
+{
+	char c;
+	int b;
+	long int a;
+	long long int d;
+	float e;
+
+	printf("Size of a char: %d byte(s)\n",sizeof(c));
+	printf("Size of a int: %d byte(s)\n",sizeof(d));
+	printf("Size of a long int: %d byte(s)\n",sizeof(a));
+	printf("Size of a long long int: %d byte(s)\n",sizeof(d));
+	printf("Size of a float: %d byte(s)\n",sizeof(e));
+}
+
