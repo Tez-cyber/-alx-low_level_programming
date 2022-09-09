5658b09c3b784dbd17ebb746b4de207c85d48593
Salami Azeez <salamitopeazeez@gmail.com>
2022-09-08 19:49:57 -0700

    Working with C

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 /**
- * main - Prints a particular set of chars
+ * main - Pritn a particular set of char
  *
- * Description: Using main function
+ * Description: Using the main function
  * Return: 1
  */
 int main(void)
 {
-	write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",59);
-	return (1);
+       	write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",59);
+        return (1);
 }
