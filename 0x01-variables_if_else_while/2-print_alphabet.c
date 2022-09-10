3ea14c534f10d8ec6419f62c15ef0b491b7f2513
Salami Azeez <salamitopeazeez@gmail.com>
2022-09-09 20:14:18 -0700

    New commit

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Printing alphabets
+ *
+ * Description: Using main function
+ * Return: 0
+ */
+int main(void)
+{
+	char ap;
+
+	for (ap = 'a'; ap <= 'z'; ap++)
+		putchar(ap);
+	return (0);
+}
