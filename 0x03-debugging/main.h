4990cbe144538128edd1f58ca2b3234c7ac59c62
Salami Azeez <salamitopeazeez@gmail.com>
2022-09-17 01:19:25 -0700

    Task on debuggging

---
+++ 0x03-debugging/main.h
@@ -0,0 +1,16 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/*
+ * File: main.h
+ * 
+ * Desc: Header file containing prototypes for all functions
+ *       used in the 0x03-debugging.
+ */
+
+void positive_or_negative(int i);
+int largest_number(int, int, int);
+void print_remaining_days(int month, int day, int year);
+int convert_day(int month, int day);
+
+#endif
