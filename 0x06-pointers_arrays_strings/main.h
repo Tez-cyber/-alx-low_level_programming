36bc2dc11db7d38d0e5c9f0d4bf52cd1835da6e9
Salami Azeez <salamitopeazeez@gmail.com>
2022-10-01 01:02:56 -0700

    New projects added

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,9 +1,8 @@
-#ifndef HOLBERTON_H
-#define HOLBERTON_H
+#ifndef MAIN_H
+#define MAIN_H
 
 /**
- * File: holberton.h
- * Auth: Brennan D Baraban
+ * 
  * Desc: Header file containing prototypes for all functions
  *       used in the 0x05-pointers_arrays_strings directory.
  */
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -2,7 +2,6 @@
 #define MAIN_H
 
 /**
- * File: holberton.h
  * Desc: Header file containing prototypes for all functions
  *       used in the 0x07-recursion directory.
  */
